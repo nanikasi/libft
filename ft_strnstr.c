d0a17248dd1415296fc70fb264989849b73aa489
nanikasi <nanikasi1024@gmail.com>
2022-09-08 19:44:56 +0900

    ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nanakani <nanakani@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 16:03:36 by nanakani          #+#    #+#             */
+/*   Updated: 2022/08/24 16:03:36 by nanakani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int		hi;
+	int		ni;
+	char	*haystack_tmp;
+	char	*needle_tmp;
+
+	hi = 0;
+	haystack_tmp = (char *)haystack;
+	needle_tmp = (char *)needle;
+	while (hi < len)
+	{
+		ni = 0;
+		while (needle_tmp[ni] != '\0' && hi + ni < len)
+		{
+			if (haystack_tmp[hi + ni] != needle_tmp[ni])
+				break ;
+			ni++;
+		}
+		if (ni == ft_strlen(needle_tmp))
+			return (&haystack_tmp[hi]);
+		hi++;
+	}
+	return (NULL);
+}
