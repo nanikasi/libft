a5af49492cd1edf39a3f867c80eceecaffec402d
Naoto Nakanishi <nanikasi1024@yahoo.co.jp>
2022-09-08 11:50:56 +0900

    ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nanakani <nanakani@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 16:03:28 by nanakani          #+#    #+#             */
+/*   Updated: 2022/08/24 16:03:28 by nanakani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int		i;
+	char	*string1;
+	char	*string2;
+
+	i = 0;
+	string1 = (char *)s1;
+	string2 = (char *)s2;
+	while (n > i)
+	{
+		if (string1[i] != string2[i] || string1[i] == '\0' || string2[i] == '\0')
+			return (string1[i] - string2[i]);
+		i ++;
+	}
+	return (0);
+}
