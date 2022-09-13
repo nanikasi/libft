ac582cf3732e734c0b3bb63bda65f06cfa679b6d
nanikasi <nanikasi1024@gmail.com>
2022-09-13 12:07:27 +0900

    ft_striteri

---
+++ ft_striteri.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nanakani <nanakani@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/10 22:45:48 by nanakani          #+#    #+#             */
+/*   Updated: 2022/09/10 22:45:48 by nanakani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int		len;
+	int		i;
+	char	*p;
+
+	len = ft_strlen(s);
+	i = 0;
+	while (i < len)
+	{
+		f(i, &s[i]);
+		i++;
+	}
+	return ;
+}
