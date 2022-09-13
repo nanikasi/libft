82d407db5a62161aa196377078817079faa8c37a
nanakani <nanakani@student.42tokyo.jp>
2022-09-13 22:16:04 +0900

    return not const value

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	haystack_tmp = (char *)haystack;
 	needle_tmp = (char *)needle;
 	if (len == 0)
-		return (haystack);
+		return (haystack_tmp);
 	while (hi < len)
 	{
 		ni = 0;
