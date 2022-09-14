528f62eb1c91ace4682536cc993e1167056d4e72
nanikasi <nanikasi1024@gmail.com>
2022-09-14 22:45:12 +0900

    done

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,25 +17,24 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	int		hi;
 	int		ni;
 	char	*haystack_tmp;
-	char	*needle_tmp;
 	size_t	haystack_len;
 
 	hi = 0;
 	haystack_len = (size_t) sizeof(haystack) / (size_t) sizeof(haystack[0]);
 	haystack_tmp = (char *)haystack;
-	needle_tmp = (char *)needle;
 	if (ft_strlen(needle) == 0)
 		return ((char *)haystack);
 	while (hi < len && hi < haystack_len)
 	{
 		ni = 0;
-		while (needle_tmp[ni] != '\0' && hi + ni < len && hi + ni <= haystack_len)
+		while (needle[ni] != '\0' && hi + ni < len
+			&& hi + ni <= haystack_len)
 		{
-			if (haystack_tmp[hi + ni] != needle_tmp[ni])
+			if (haystack[hi + ni] != needle[ni])
 				break ;
 			ni++;
 		}
-		if (ni == ft_strlen(needle_tmp))
+		if (ni == ft_strlen(needle))
 			return (&haystack_tmp[hi]);
 		hi++;
 	}
